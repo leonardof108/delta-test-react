29d23e800f1bf90186ac1a25db1b6bad837fa31f
leonardof108 <leonardof108@gmail.com>
2023-08-10 21:58:46 -0300

    Banner resizing fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "65c51d0c39d46246"; }
+/******/ 		__webpack_require__.h = function() { return "4a0822135a044738"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
